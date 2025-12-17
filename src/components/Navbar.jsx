49de97b23757bc4973c461fa7c032ae03fe2f26e
sashetty1 <shrivatsa.ashetty@ironmountain.com>
2025-12-17 14:43:44 +0530

    completed building the Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+function Navbar() {
+  return (
+    <>
+      <nav
+        className="bg-purple-800
+            text-white 
+            flex 
+            justify-around 
+            font-semibold
+            p-2.5
+            
+            /* Mobile Styles */
+            max-sm:flex-col      /* Stacks items vertically */
+            max-sm:items-center  /* Centers items horizontally */
+            max-sm:gap-3         /* Adds space between Logo and List */
+            max-sm:font-medium
+            max-sm:text-sm
+            "
+      >
+        <span className="text-lg hover:font-bold ">iTask</span>
+
+        <ul className="list-none flex gap-5 
+            /* Mobile Styles */
+            max-sm:flex-col 
+            max-sm:items-center /* Centers the links in the stack */
+            max-sm:gap-2        /* Reduces gap between links on mobile */
+            ">
+          <li className="hover:font-bold transition-[font-weight] duration-300 ease-in-out">
+            <a href="#">Home</a>
+          </li>
+          <li className="hover:font-bold transition-[font-weight] duration-300 ease-in-out">
+            <a href="#">YourTasks</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
